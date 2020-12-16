5c2c9724bd23da954b64c17bc2f76437c5810779
duan <nubitol45@gmail.com>
2020-12-16 20:20:09 +0700

    add file Dummy

---
+++ app/Kasut/dummy.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Kasut;
+
+final class dummy
+{
+    public static function brands()
+    {
+        $brands = [
+            'Kodachi', 'Geoff Max', 'NAH Project', 'Brodo', 'Compass', 'Forever Young Crew', 'Saint Barkley', 'Word Division', 'League', 'ARL Footwear', 'Wakai', 'Piero', 'Eagle', 'Buccheri', 'Yongki Komaladi', 'Ardiles', 'Footstep Footwear',
+        ];
+        return $brands;
+    }
+
+    public static function categories()
+    {
+        $categories = ['Sneaker Pria', 'Slip On Pria', 'Boots Pria', 'Pantofel Pria', 'Sandal Pria', 'Loafers Pria', 'Sepatu Sandal Pria', 'Boots Wanita', 'Espadrilles Wanita', 'Flat Shoes Wanita', 'Loafers Wanita', 'Pantofel Wanita', 'Sandal Wanita', 'Sepatu Heels Wanita', 'Sepatu Sandal Wanita', 'Slip On Wanita', 'Sneaker Wanita', 'Wedges Wanita',];
+        return $categories;
+    }
+
+    public static function profiles()
+    {
+        $profiles = ['pic1.jpg', 'pic2.png'];
+        return $profiles;
+    }
+
+    public static function itemNames()
+    {
+        $items = ['Sepatu Kodachi International', 'Sepatu Capung - Kodachi 8119 Star Cefron', 'Sepatu Sekolah Warior Compass tinggi dan pendek', 'NAH X EVOS Legendary Sepatu Sneakers NAH Project Original', 'sepatu BRODO', 'Sepatu Compass Gazelle', 'Sneackers Geoff Max Avalon Red Rubic', 'Promo Heboh Sepatu Nah Project Yas Flexknit 2.0', 'BRODO Hawk', 'Sepatu Compass Gazelle Low', 'Word Division Classic', 'Best Seller League Sepatu Sneakers Pria Vault 2.0', 'SEPATU BLACKMASTER ARL FOOTWEAR', 'Wakai Footwear Pria Sneakers Snm01802 Uwabaki', 'Wakai Footwear Pria Slip-On FM11836 Gyou', 'Footwear Pria Slip-On Wakai FM11817 Seiryoku Mustard', 'Sepatu casual piero Vamos', 'sepatu casual piero flow B', 'Eagle Laguna Lifestyle', 'Eagle Laguna Lifestyle Shoes', 'Sepatu Eagle Aero Running Shoes', 'SEPATU BUCCHERI', 'Special Edition Sepatu Buccheri Mgr-04', 'Buccheri Kent Sneakers', 'Yongki Komaladi', 'Sepatu Running Ardiles Kayapo', 'Sepatu Slip On Ardiles Kenwood', 'Sepatu Sneakers Footstep Footwear', 'Sepatu Handmade Footstep Footwear', 'Sepatu Capung - Kodachi 8119 Star Cefron', 'NAH X EVOS Legendary Sepatu Sneakers NAH Project'];
+        return $items;
+    }
+
+    public static function colorNames()
+    {
+        $colors = ['Pink', 'Crimson', 'Red', 'Maroon', 'Brown', 'Misty Rose', 'Salmon', 'Coral', 'Orange-Red', 'Chocolate', 'Orange', 'Gold', 'Yellow', 'Olive', 'Yellow-Green', 'Lawn green', 'Chartreuse', 'Lime', 'Green', 'Spring green', 'Aquamarine', 'Turquoise', 'Aqua/Cyan', 'Teal', 'Lavender', 'Blue', 'Navy', 'Indigo', 'Dark Violet', 'Plum', 'Magenta', 'Purple', 'Red-Violet', 'Tan', 'Beige', 'Slate gray', 'White', 'Silver', 'Grey', 'Black'];
+        return $colors;
+    }
+
+    public static function itemPhotos()
+    {
+        $photo = ['item1.jpeg', 'item1.jpg', 'item2.jpeg', 'item2.jpg', 'item3.jpeg', 'item3.jpg', 'item4.jpeg', 'item4.jpg', 'item5.jpeg', 'item5.jpg', 'item6.jpeg', 'item6.jpg', 'item7.jpeg', 'item7.jpg', 'item8.jpeg', 'item8.jpg', 'item9.jpeg', 'item9.jpg', 'item10.jpeg', 'item10.jpg', 'item11.jpeg', 'item11.jpg', 'item12.jpeg', 'item12.jpg', 'item13.jpeg', 'item13.jpg', 'item14.jpeg', 'item14.jpg', 'item15.jpeg', 'item15.jpg', 'item16.jpeg', 'item16.jpg', 'item17.jpeg', 'item17.jpg', 'item18.jpeg', 'item18.jpg', 'item19.jpeg', 'item19.jpg', 'item20.jpeg', 'item20.jpg', 'item21.jpeg', 'item21.jpg', 'item22.jpeg', 'item22.jpg',];
+        return $photo;
+    }
+}
