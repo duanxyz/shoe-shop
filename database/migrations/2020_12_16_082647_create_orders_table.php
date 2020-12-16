fd8c7177688030cc55923f5f0238c6204dab49b8
duan <nubitol45@gmail.com>
2020-12-16 16:21:50 +0700

    add migration orders

---
+++ database/migrations/2020_12_16_082647_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->String('invoiceNo')->nullable()->unique();
+            $table->foreignId('customer_id');
+            $table->foreign('customer_id')
+                ->references('id')
+                ->on('customers')
+                ->onDelete('cascade');
+            $table->bigInteger('payment')->nullable();
+            $table->bigInteger('grand_total')->nullable();
+            $table->string('status')->default('troli');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
