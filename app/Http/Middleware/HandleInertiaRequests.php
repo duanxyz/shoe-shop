2b3dd9ecf69c86c490e60902a0023eb5bb99da0e
duan <nubitol45@gmail.com>
2020-12-30 23:05:26 +0700

    fix after upgrade jetstream

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Session;
 use Inertia\Middleware;
+use Laravel\Fortify\Features;
+use Laravel\Jetstream\Jetstream;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -37,7 +43,47 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'jetstream' => function () use ($request) {
+                return [
+                    'canCreateTeams' => $request->user() &&
+                        Jetstream::hasTeamFeatures() &&
+                        Gate::forUser($request->user())->check('create', Jetstream::newTeamModel()),
+                    'canManageTwoFactorAuthentication' => Features::canManageTwoFactorAuthentication(),
+                    'canUpdatePassword' => Features::enabled(Features::updatePasswords()),
+                    'canUpdateProfileInformation' => Features::canUpdateProfileInformation(),
+                    'flash' => $request->session()->get('flash', []),
+                    'hasAccountDeletionFeatures' => Jetstream::hasAccountDeletionFeatures(),
+                    'hasApiFeatures' => Jetstream::hasApiFeatures(),
+                    'hasTeamFeatures' => Jetstream::hasTeamFeatures(),
+                    'hasTermsAndPrivacyPolicyFeature' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+                    'managesProfilePhotos' => Jetstream::managesProfilePhotos(),
+                ];
+            },
+            'user' => function () use ($request) {
+                if (!$request->user()) {
+                    return;
+                }
+
+                if (Jetstream::hasTeamFeatures() && $request->user()) {
+                    $request->user()->currentTeam;
+                }
+
+                return array_merge($request->user()->toArray(), array_filter([
+                    'all_teams' => Jetstream::hasTeamFeatures() ? $request->user()->allTeams() : null,
+                ]), [
+                    'two_factor_enabled' => !is_null($request->user()->two_factor_secret),
+                ]);
+            },
+            'errorBags' => function () {
+                return collect(optional(Session::get('errors'))->getBags() ?: [])->mapWithKeys(function ($bag, $key) {
+                    return [$key => $bag->messages()];
+                })->all();
+            },
+            'carts' => function () use ($request) {
+                if (Auth::id()) {
+                    return Customer::where('user_id', Auth::id())->first()->cart->items;
+                }
+            }
         ]);
     }
 }
--- resources/js/Jetstream/Dropdown.vue
+++ resources/js/Jetstream/Dropdown.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="relative">
-        <div @click="open = ! open">
+        <div @mouseover="open = true" @mouseleave="open = false">
             <slot name="trigger"></slot>
         </div>
 
         <!-- Full Screen Dropdown Overlay -->
-        <div v-show="open" class="fixed inset-0 z-40" @click="open = false">
-        </div>
+        <!-- <div v-show="open" class="fixed inset-0 z-40" @click="open = false">
+        </div> -->
 
         <transition
             enter-active-class="transition ease-out duration-200"
@@ -14,13 +14,17 @@
             enter-to-class="transform opacity-100 scale-100"
             leave-active-class="transition ease-in duration-75"
             leave-class="transform opacity-100 scale-100"
-            leave-to-class="transform opacity-0 scale-95">
-            <div v-show="open"
-                    class="absolute z-50 mt-2 rounded-md shadow-lg"
-                    :class="[widthClass, alignmentClasses]"
-                    style="display: none;"
-                    @click="open = false">
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
+            leave-to-class="transform opacity-0 scale-95"
+        >
+            <div
+                v-show="open"
+                class="absolute z-50 mt-2 rounded-md shadow-lg"
+                :class="[widthClass, alignmentClasses, marginClass]"
+                style="display: none"
+                @mouseover="open = true"
+                @mouseleave="open = false"
+            >
+                <div class="rounded-md shadow-xs" :class="contentClasses">
                     <slot name="content"></slot>
                 </div>
             </div>
@@ -30,54 +34,67 @@
 
 <script>
     export default {
-        props: {
-            align: {
-                default: 'right'
-            },
-            width: {
-                default: '48'
-            },
-            contentClasses: {
-                default: () => ['py-1', 'bg-white']
-            }
+    props: {
+        align: {
+            default: "right",
         },
+        width: {
+            default: "48",
+        },
+        margin: {
+            default: "5",
+        },
+        contentClasses: {
+            default: () => ["py-1", "bg-white"],
+        },
+    },
 
-        data() {
+    data() {
+        return {
+            open: false,
+        };
+    },
+
+    created() {
+        const closeOnEscape = (e) => {
+            if (this.open && e.keyCode === 27) {
+                this.open = false;
+            }
+        };
+
+        this.$once("hook:destroyed", () => {
+            document.removeEventListener("keydown", closeOnEscape);
+        });
+
+        document.addEventListener("keydown", closeOnEscape);
+    },
+
+    computed: {
+        widthClass() {
             return {
-                open: false
-            }
+                48: "w-48",
+                96: "w-96",
+            }[this.width.toString()];
+        },
+        marginClass() {
+            return {
+                5: "-mr-5",
+                20: "-mr-20",
+                40: "-mr-40",
+                60: "-mr-60",
+            }[this.margin.toString()];
         },
 
-        created() {
-            const closeOnEscape = (e) => {
-                if (this.open && e.keyCode === 27) {
-                    this.open = false
-                }
+        alignmentClasses() {
+            console.log(this.align);
+            if (this.align == "left") {
+                return "origin-top-left left-0";
+            } else if (this.align == "right") {
+                return "origin-top-right right-0";
+            } else {
+                return "origin-top";
             }
-
-            this.$once('hook:destroyed', () => {
-                document.removeEventListener('keydown', closeOnEscape)
-            })
-
-            document.addEventListener('keydown', closeOnEscape)
         },
-
-        computed: {
-            widthClass() {
-                return {
-                    '48': 'w-48',
-                }[this.width.toString()]
-            },
-
-            alignmentClasses() {
-                if (this.align == 'left') {
-                    return 'origin-top-left left-0'
-                } else if (this.align == 'right') {
-                    return 'origin-top-right right-0'
-                } else {
-                    return 'origin-top'
-                }
-            },
-        }
+    },
     }
 </script>
