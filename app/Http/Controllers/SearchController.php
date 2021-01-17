2b96adfc363384665d914ffff7f542c56638e01c
duan <nubitol45@gmail.com>
2021-01-17 16:59:20 +0700

    add SearchController

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    protected $item;
+
+    public function __construct(Item $item)
+    {
+        $this->item = $item;
+    }
+
+    public function index(Request $request)
+    {
+        $category = null;
+
+        $query = $this->item->query()->with('category');
+        if (!empty($request->min_price)) {
+            $query->where('price', '>=', $request->min_price);
+        }
+
+        if (!empty($request->max_price)) {
+            $query->where('price', '<=', $request->max_price);
+        }
+
+        if (!empty($request->condition)) {
+            $query->where('condition', $request->condition);
+        }
+
+        if (!empty($request->search)) {
+            $query->where('name', 'like', '%' . $request->search . '%')
+                ->orWhere('description', 'like', '%' . $request->search . '%');
+        }
+
+        if (!empty($request->category)) {
+            $category = $request->category;
+        } else {
+            $category = $request->search;
+        }
+
+        $query->whereHas('category', function ($q) use ($category) {
+            $q->where('name', 'like', '%' . $category . '%');
+        });
+
+        if ($request->low) {
+            $query->orderBy('price', 'asc');
+        }
+
+        if ($request->height) {
+            $query->orderBy('price', 'desc');
+        }
+
+        if ($request->latest) {
+            $query->orderBy('created_at', 'desc');
+        }
+
+        return $query->get();
+    }
+}
