69db9fd3db519d0b948ddcfc29e1bed44cb2222d
duan <nubitol45@gmail.com>
2021-01-18 00:43:39 +0700

    add component SortItems

---
+++ resources/js/components/SortItems.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex gap-2 items-center">
+        <label for="sort">
+            <b for="sort">urutkan :</b>
+        </label>
+        <select name="sort" class="text-sm rounded-md">
+            <option value="paling sesuai">Paling Sesuai</option>
+            <option value="latest">Terbaru</option>
+            <option value="max_price">Harga Tertinggi</option>
+            <option value="min_price">Harga Terendah</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
